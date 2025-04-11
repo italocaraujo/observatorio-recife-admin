2c4edfcbc527ff527b27349e94ea7f279ade8ddb
Italo Correia <italocdaraujo@gmail.com>
2025-04-11 11:53:58 -0300

    fix: edit news modal style

--- src/components/news/EditNews.tsx
+++ src/components/news/EditNews.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { handleSave } from '@/@api/http/news/newsActions';
-import styles from '@/app/styles/news/CreateNews.module.css';
+import styles from '@/app/styles/news/EditNews.module.css';
+import additionalStyles from '@/app/styles/news/CreateNews.module.css';
 
 interface NewsItem {
   id: number;
@@ -72,10 +73,17 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
         <div className={styles.modalBackdrop}>
           <div className={styles.modalContainer}>
             <div className={styles.modalContent}>
-              <h1 className={styles.titleCreateNews}>Editar Notícia</h1>
+              <div className={styles.modalHeader}>
+                <h1 className={styles.titleCreateNews}>Editar Notícia</h1>
+                <button className={styles.closeButtonX} onClick={onCancel}>
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"/>
+                  </svg>
+                </button>
+              </div>
 
-              <form onSubmit={handleSubmit} className={styles.formCreateNews}>
-                <div className={styles.inputContainer}>
+              <form onSubmit={handleSubmit} className={additionalStyles.formCreateNews}>
+                <div className={additionalStyles.inputContainer}>
                   <label htmlFor="title">Título</label>
                   <input
                     type="text"
@@ -85,7 +93,7 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
                   />
                 </div>
 
-                <div className={styles.inputContainer}>
+                <div className={additionalStyles.inputContainer}>
                   <label htmlFor="description">Descrição</label>
                   <textarea
                     id="description"
@@ -94,7 +102,7 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
                   />
                 </div>
 
-                <div className={styles.inputContainer}>
+                <div className={additionalStyles.inputContainer}>
                   <label htmlFor="date">Data</label>
                   <input
                     type="date"
@@ -104,7 +112,7 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
                   />
                 </div>
 
-                <div className={styles.inputContainer}>
+                <div className={additionalStyles.inputContainer}>
                   <label htmlFor="link">Link</label>
                   <input
                     type="url"
@@ -114,7 +122,7 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
                   />
                 </div>
 
-                <div className={styles.inputContainer}>
+                <div className={additionalStyles.inputImageContainer}>
                   <label htmlFor="image">Alterar Imagem</label>
                   <input
                     type="file"
@@ -123,11 +131,11 @@ const EditNews: React.FC<EditNewsProps> = ({ newsToEdit, onCancel, isOpen, setEr
                   />
                 </div>
 
-                <div className={styles.formActions}>
-                  <button type="button" onClick={onCancel} className={styles.resetButton}>
+                <div className={additionalStyles.formActions}>
+                  <button type="button" onClick={onCancel} className={additionalStyles.resetButton}>
                     Cancelar
                   </button>
-                  <button type="submit" className={styles.submitButton}>
+                  <button type="submit" className={additionalStyles.submitButton}>
                     Salvar Alterações
                   </button>
                 </div>
